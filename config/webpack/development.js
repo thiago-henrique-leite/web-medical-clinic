f0de4eec83ba3a3ee7bd442a7541c89f920a8b7f
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-02-13 02:44:07 -0300

    up

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,3 +1,9 @@
+// I Had to change:
+import DoISuportIt from 'components/DoISuportIt';
+
+// to (notice the missing `./`)
+import DoISuportIt from './components/DoISuportIt';
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
