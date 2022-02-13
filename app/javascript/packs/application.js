595a84accaf376a17d81380aaa2a69ea95ea4779
Thiago Henrique Leite da Silva <thiago.leitesilva05@gmail.com>
2022-02-13 03:23:57 -0300

    up

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "./stimulus"
+import { definitionsFromContext } from "./stimulus/webpack-helpers"
 
 import "bootstrap"
 import "../stylesheets/application"
